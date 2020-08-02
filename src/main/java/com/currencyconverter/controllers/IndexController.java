9f1f6f0d1da389b4a8e8268fdbc6ee131355d6ed
armankostandov <armankostandov@gmail.com>
2020-08-02 21:33:56 +0300

    add converter template to index page

--- src/main/java/com/currencyconverter/controllers/IndexController.java
+++ src/main/java/com/currencyconverter/controllers/IndexController.java
@@ -1,15 +1,25 @@
 package com.currencyconverter.controllers;
 
+import com.currencyconverter.services.CurrencyService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping({"", "/", "index", "index.html"})
-    public String index() {
+    CurrencyService currencyService;
 
+    public IndexController(CurrencyService currencyService) {
+        this.currencyService = currencyService;
+    }
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(Model model) {
+
+        model.addAttribute("currencies", currencyService.findAll());
         return "index";
+
     }
 }
 
